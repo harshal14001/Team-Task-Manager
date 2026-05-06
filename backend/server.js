401885e580b1c1c98a3b0de69becf30e8009cf79
harshal14001 <harshalargade2001@gmail.com>
2026-05-07 01:08:39 +0530

    Updates

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ Connected to MongoDB successfully!');
     app.listen(PORT, () => {
